4f646d589a585931d63e23fdae0bae88624636e3
Hanner <30414721+zerodaisies@users.noreply.github.com>
2024-07-12 12:09:01 -0500

    fix typo, remove target groups

--- main.tf
+++ main.tf
@@ -69,13 +69,7 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
         }
-      }
-    }
   ]
 
   http_tcp_listeners = [
